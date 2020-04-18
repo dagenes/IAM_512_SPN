36a513b1f8eaab2f1c27b6a86f036d452a822cc2
dagenes <dagenes1@gmail.com>
2020-04-18 18:30:12 +0300

    first commit

---
+++ IAM_512_SPN/src/main.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include "Converter.h"
+#include "SPN.h"
+
+
+using namespace std;
+
+
+
+int
+main ()
+{
+    Converter c;
+    string s = c.strToBin("dsafdsaf");
+    cout << "s is " << s;
+    
+//     SPN spn ;
+
+  return 0;
+}
+
+
